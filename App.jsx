fe01351b6217887651fc7d71fed9a553b751ebe4
Eskndr <eskndrytbarek323@gmail.com>
2024-11-26 10:54:15 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+import Card from './Card.jsx' ;
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <Card Name= "jack" Age="16" isEnrolled ={false}/>
+      <Card Name= "Buck" Age="22" isEnrolled ={true}/>
+      <Card Name= "Gina" Age="21" isEnrolled ={true}/>
+      <Card/>
+      
+      </>
+  );
+}
+
+export default App;
